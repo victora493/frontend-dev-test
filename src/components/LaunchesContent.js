db050dfe66acc5423ebe051b42ce89560b04289d
victora493 <71620855+victora493@users.noreply.github.com>
2021-05-02 23:14:55 -0700

    made search bar a component

--- src/components/LaunchesContent.js
+++ src/components/LaunchesContent.js
@@ -4,6 +4,7 @@ import { useHistory, useLocation } from 'react-router-dom';
 import axios from 'axios';
 import Launch from '../components/Launch';
 import Truncate from 'react-truncate'
+import SearchBar from './SearchBar';
 
 
 export default function LaunchesContent() {
@@ -96,19 +97,7 @@ export default function LaunchesContent() {
     
     return (
         <>
-            <div className={styles.searchWrapper}>
-                <input 
-                    type="text" 
-                    placeholder="Search by Rocket Name..."
-                    onChange={handleInputChange}
-                />
-                <svg class="Icon Icon--search-desktop" role="presentation" viewBox="0 0 21 21">
-                    <g transform="translate(1 1)" stroke="currentColor" stroke-width="2" fill="none" fill-rule="evenodd" stroke-linecap="square">
-                        <path d="M18 18l-5.7096-5.7096"></path>
-                        <circle cx="7.2" cy="7.2" r="7.2"></circle>
-                    </g>
-                </svg>
-            </div>
+            <SearchBar/>
             <div className={styles.ContentWrapper}>
                 <div class={styles.dataList}>
                     <h1>Records</h1>
---
+++ src/components/SearchBar.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import styles from './SearchBar.module.css'
+
+export default function SearchBar() {
+    const [term, setTerm] = useState('')
+
+    return (
+        <div className={styles.searchWrapper}>
+            <input 
+                type="text" 
+                placeholder="Search by Rocket Name..."
+                value={term}
+                onChange={e => setTerm(e.target.value)}
+            />
+            <svg class="Icon Icon--search-desktop" role="presentation" viewBox="0 0 21 21">
+                <g transform="translate(1 1)" stroke="currentColor" stroke-width="2" fill="none" fill-rule="evenodd" stroke-linecap="square">
+                    <path d="M18 18l-5.7096-5.7096"></path>
+                    <circle cx="7.2" cy="7.2" r="7.2"></circle>
+                </g>
+            </svg>
+        </div>
+    )
+}
